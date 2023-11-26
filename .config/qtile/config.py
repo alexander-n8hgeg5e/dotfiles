461622f4e2c83cef8baa29f8f77333311c6fc96d
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2023-11-26 18:27:22 +0100

    remove unused and interfering

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -190,20 +190,20 @@ keys = [
 
 groups = [Group(i) for i in "abcd"]
 
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key(CA, i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key(CAS , i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key(mod + [ "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-    ])
+#for i in groups:
+#    keys.extend([
+#        # mod1 + letter of group = switch to group
+#        Key(CA, i.name, lazy.group[i.name].toscreen(),
+#            desc="Switch to group {}".format(i.name)),
+#
+#        # mod1 + shift + letter of group = switch to & move focused window to group
+#        Key(CAS , i.name, lazy.window.togroup(i.name, switch_group=True),
+#            desc="Switch to & move focused window to group {}".format(i.name)),
+#        # Or, use below if you prefer not to switch to that group.
+#        # # mod1 + shift + letter of group = move focused window to group
+#        # Key(mod + [ "shift"], i.name, lazy.window.togroup(i.name),
+#        #     desc="move focused window to group {}".format(i.name)),
+#    ])
 
 layouts = [
     #layout.Columns(border_focus_stack='#d75f5f'),
